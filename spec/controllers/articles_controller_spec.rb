a5891d2fb49afcb700f8fc2c054348871df0f268
John <john@futureworkz.com>
2014-12-01 09:21:48 +0700

    add spec AritclesController#new

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 describe ArticlesController do
-  
+  describe 'GET #new' do
+    def do_request
+      get :new
+    end
+
+    it 'render to view:new' do
+      do_request
+      expect(response).to render_template :new
+    end
+  end 
 end
